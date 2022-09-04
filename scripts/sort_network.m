52418d25fb75a9c32c6ccd7cb194b7e4e880ff14
XieCong0330 <106384775+XieCong0330@users.noreply.github.com>
2022-09-04 17:12:15 +0800

    Update sort_network.m

--- scripts/sort_network.m
+++ scripts/sort_network.m
@@ -1,14 +1,14 @@
 function g1 = sort_network(g)
 % g1 = sort_network(g),for N by N matrix, returns M (number of non-zero
-% elements) by 3 double with a ascend order by the weights of the edges
-% In ENT, there would be some negative value given the postion of stimuls
-% are randomly assigned(if participants indicates the stimuls on the left 
+% elements) by 3 double with a ascending order by the weights of the edges
+% In ENT, there would be some negative value given the position of stimuli
+% are randomly assigned(if participants indicate the stimuli on the left 
 % part is better than that on the right part in ENT, the value would be negative),
 % here, the negative values would be transformed to positive values with a
-% reversed postion(e.g., if -0.4 is located in row 4, column 2, it would be 
-% transformed to 0.4 with the location of row 2,column 4)  
+% reversed position(e.g., if -0.4 is located in row 4, column 2, it would be 
+% transformed to 0.4 with the location of row 2, column 4)  
 % the first column of g1 is the ids of the source nodes, the
-% sconed column of g1 is the ids of the target nodes, and the third
+% sconed column of g1 is the ids of the target nodes and the third
 % column is the weights of edges.
 a = ones(size(g))*0;
 for m = 1:size(a,1)
